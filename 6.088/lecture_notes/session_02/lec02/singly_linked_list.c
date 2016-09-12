cb5c8a4f5547abd51a2e573dedc00dd5943616eb
M <lmiguelvargasf@gmail.com>
2016-09-12 13:54:01 -0500

    Make sure memory allocation was done successfully

--- 6.088/lecture_notes/session_02/lec02/singly_linked_list.c
+++ 6.088/lecture_notes/session_02/lec02/singly_linked_list.c
@@ -5,45 +5,52 @@
 /* Returns a new node with the given value. */
 node_t* make_node(int value) {
   node_t* new_node = malloc(sizeof(node_t));
-  new_node->value = value;
-  new_node->next = NULL;
+
+  if (new_node != NULL) {
+    new_node->value = value;
+    new_node->next = NULL;
+  }
+
   return new_node;
 }
 
 /* Insert at the head of a linked list. */
-node_t* insert_value(int value, node_t* cur_head) {
-  node_t* new_node = make_node(value);
-  new_node->next = cur_head;
-  return new_node;
+node_t* insert_value(int value, node_t* current_head) {
+  node_t* new_head = make_node(value);
+  if (new_head != NULL) {
+    new_head->next = current_head;
+  }
+
+  return new_head;
 }
 
 /* Deletes the first occurence of value from the list. */
-node_t* delete_value(int value, node_t* cur_head, int* succeeded) {
+node_t* delete_value(int value, node_t* current_head, int* succeeded) {
   *succeeded = 0;
 
   /* Special case: if first guy is the one we want to remove. */
-  if (cur_head == NULL) {
+  if (current_head == NULL) {
     return NULL;
-  } else if (cur_head->value == value) {
-    node_t* new_head = cur_head->next;
-    free(cur_head);
+  } else if (current_head->value == value) {
+    node_t* new_head = current_head->next;
+    free(current_head);
     *succeeded = 1;
     return new_head;
   } else {
-    node_t* prev = cur_head;
-    node_t* cur = cur_head->next;
+    node_t* prev = current_head;
+    node_t* cur = current_head->next;
     while (cur != NULL) {
       if (cur->value == value) {
         prev->next = cur->next;
         free(cur);
         *succeeded = 1;
-        return cur_head;
+        return current_head;
       } else {
         cur = cur->next;
       }
     }
   }
-  return cur_head;
+  return current_head;
 }
 
 /* Frees the memory associated with a linked list. */
