02a33a586aef7b404a4d7f1b410a9561bec17c22
M <lmiguelvargasf@gmail.com>
2016-09-13 10:42:15 -0500

    Rename file

--- 6.088/assignments/session_03/binary_search_tree.h
+++ 6.088/assignments/session_03/binary_search_tree.h
@@ -3,35 +3,35 @@
 /*
  * Define a tree structure.
  */
-typedef struct bst {
-  int* vals;
+typedef struct binary_search_tree {
+  int* values;
   int size;
-} bst_t;
+} binary_search_tree_t;
 
-/* Returns a bst_t structure with an int array vals of size "size" and the
+/* Returns a binary_search_tree_t structure with an int array values of size "size" and the
  * size field set appropriately. */
-bst_t* make_tree(int size);
+binary_search_tree_t* make_tree(int size);
 
 /*
  * Initialize the binary search tree.
  * HINT: What should we initialize values to?  What requirements might we have
  * to add to the tree specification?
  */
-void init_tree(bst_t* tree);
+void init_tree(binary_search_tree_t* tree);
 
 /*
  * Inserts a new value into a given tree.
  */
-void insert(int val, bst_t* tree);
+void insert(int value, binary_search_tree_t* tree);
 
-bool find_val(int val, bst_t* tree);
+bool find_value(int value, binary_search_tree_t* tree);
 
 /*
  * Given a pointer to the root, frees the memory associated with an entire list.
  */
-void delete_bst(bst_t* tree);
+void delete_binary_search_tree(binary_search_tree_t* tree);
 
 /* Given a pointer to the root, prints all of the values in a list. */
-void print_bst(bst_t* tree);
+void print_binary_search_tree(binary_search_tree_t* tree);
 
 #endif
