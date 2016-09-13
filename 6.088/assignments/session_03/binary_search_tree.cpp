1a2fbb7bbbfbd1ceac87f96cbb17a5aec3fe296f
M <lmiguelvargasf@gmail.com>
2016-09-13 11:12:13 -0500

    Implement function to initialize tree

--- 6.088/assignments/session_03/binary_search_tree.cpp
+++ 6.088/assignments/session_03/binary_search_tree.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <climits>
 #include "binary_search_tree.h"
 
 /* Returns a binary_search_tree_t structure with an int array values of size
@@ -16,4 +17,21 @@ binary_search_tree_t* make_tree(int size) {
   }
 
   return tree;
+}
+
+/*
+ * Initialize the binary search tree.
+ */
+void initialize_tree(binary_search_tree_t* tree) {
+  for (int i = 0; i < tree->size; ++i) {
+    tree->values[i] = INT_MIN;
+  }
+}
+
+/* Given a pointer to the root, prints all of the values in a list. */
+void print_binary_search_tree(binary_search_tree_t* tree) {
+  for (int i = 0; i < tree->size; ++i) {
+    printf("%d\t", tree->values[i]);
+  }
+  puts("");
 }
