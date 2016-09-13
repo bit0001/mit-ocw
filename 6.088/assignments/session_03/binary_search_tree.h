dab51d78b98b245a19aded962db4fc04e0716c04
M <lmiguelvargasf@gmail.com>
2016-09-13 11:11:16 -0500

    Add specification

--- 6.088/assignments/session_03/binary_search_tree.h
+++ 6.088/assignments/session_03/binary_search_tree.h
@@ -2,6 +2,7 @@
 #define BINARY_SEARCH_TREE
 /*
  * Define a tree structure.
+ * The range of values that can be used is INT_MIN < value <= INT_MAX
  */
 typedef struct binary_search_tree {
   int* values;
@@ -17,7 +18,7 @@ binary_search_tree_t* make_tree(int size);
  * HINT: What should we initialize values to?  What requirements might we have
  * to add to the tree specification?
  */
-void init_tree(binary_search_tree_t* tree);
+void initialize_tree(binary_search_tree_t* tree);
 
 /*
  * Inserts a new value into a given tree.
