21e4a30b15338278a18b74c56eba6043621832be
M <lmiguelvargasf@gmail.com>
2016-09-14 10:34:35 -0500

    Define BankAccount class

---
+++ 6.088/lecture_notes/session_04/playground/bank-account/bank_account.h
@@ -0,0 +1,16 @@
+#ifndef BANK_ACCOUNT_H
+#define BANK_ACCOUNT_H
+
+class BankAccount {
+private:
+  std::string m_name;
+  double m_balance;
+public:
+  BankAccount();
+  BankAccount(std::string name, double balance);
+  void display_information();
+  void deposit(int amount);
+  void withdraw(int amount);
+}
+
+#endif
