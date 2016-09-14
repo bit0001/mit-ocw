af313f127d334e76206cd3365ac78fce9e4311bc
M <lmiguelvargasf@gmail.com>
2016-09-14 12:42:07 -0500

    Add helpers

---
+++ 6.088/assignments/session_04/bst_struct.cpp
@@ -0,0 +1,74 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "bst_struct.h"
+
+/*
+ * Creates a new node from a given value, allocating heap memory for it.
+ */
+node_t* make_tree_node(int value) {
+  node_t* new_tree_node = (node_t*) malloc(sizeof(node_t));
+
+  if (new_tree_node != NULL) {
+    new_tree_node->value = value;
+    new_tree_node->left_node = NULL;
+    new_tree_node->right_node = NULL;
+  }
+
+  return new_tree_node;
+}
+
+/*
+ * Inserts a new value into a given binary search tree, allocating heap memory
+ * for it.
+ */
+node_t* insert_value(int value, node_t* current_root) {
+  if (current_root->value > value) {
+    if (current_root->left_node == NULL) {
+      current_root->left_node = make_tree_node(value);
+      return current_root;
+    }
+    insert_value(value, current_root->left_node);
+  } else {
+    if (current_root->right_node == NULL) {
+      current_root->right_node = make_tree_node(value);
+      return current_root;
+    }
+    insert_value(value, current_root->right_node);
+  }
+  return current_root;
+}
+
+bool find_value(int value, node_t* root) {
+  if (root == NULL) {
+    return false;
+  }
+  if (root->value == value) {
+    return true;
+  } else if (root->value > value) {
+    return find_value(value, root->left_node);
+  } else {
+    return find_value(value, root->right_node);
+  }
+}
+
+/*
+ * Given a pointer to the root, frees the memory associated with an entire tree.
+ */
+void delete_binary_search_tree(node_t* root) {
+  if (root == NULL) {
+    return;
+  }
+  delete_binary_search_tree(root->left_node);
+  delete_binary_search_tree(root->right_node);
+  free(root);
+}
+
+/* Given a pointer to the root, prints all o fthe values in a tree. */
+void print_binary_search_tree(node_t* root) {
+  if (root != NULL) {
+    printf("%d ", root->value);
+    print_binary_search_tree(root->left_node);
+    print_binary_search_tree(root->right_node);
+  }
+  return;
+}
---
+++ 6.088/assignments/session_04/bst_struct.h
@@ -0,0 +1,34 @@
+#ifndef BST_STRUCT_H
+#define BST_STRUCT_H
+
+/*
+ * Define a node structure.
+ */
+typedef struct node {
+  int value;
+  struct node* left_node;
+  struct node* right_node;
+} node_t;
+
+/*
+ * Creates a new node from a given value, allocating heap memory for it.
+ */
+node_t* make_tree_node(int value);
+
+/*
+ * Inserts a new value into a given binary search tree, allocating heap memory
+ * for it.
+ */
+node_t* insert_value(int value, node_t* current_root);
+
+bool find_value(int value, node_t* root);
+
+/*
+ * Given a pointer to the root, frees the memory associated with an entire list.
+ */
+void delete_binary_search_tree(node_t* root);
+
+/* Given a pointer to the root, prints all of the values in a tree. */
+void print_binary_search_tree(node_t* root);
+
+#endif
