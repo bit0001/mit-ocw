242e0626e13e92b5e7e311b1698790fe97236064
M <lmiguelvargasf@gmail.com>
2016-09-14 11:16:23 -0500

    Define Person class

---
+++ 6.088/lecture_notes/session_04/playground/person/person.h
@@ -0,0 +1,19 @@
+#ifndef PERSON_H
+#define PERSON_H
+
+class Person {
+private:
+  static const int LIMIT = 25;
+  std::string m_lastname;
+  char m_first_name[LIMIT];
+
+public:
+  Person();
+  Person(std::string lastname);
+  Person(char * first_name);
+  Person(std::string lastname, char * first_name);
+  void show();
+  void show_formally();
+};
+
+#endif
