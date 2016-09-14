c95a1d82e07ab9efb7cb107d41c7755b32a7f67c
M <lmiguelvargasf@gmail.com>
2016-09-14 11:02:32 -0500

    Test another feature

--- 6.088/lecture_notes/session_04/playground/bank-account/test_ba.cpp
+++ 6.088/lecture_notes/session_04/playground/bank-account/test_ba.cpp
@@ -2,8 +2,17 @@
 #include "bank_account.h"
 
 int main() {
-  BankAccount john;
-  john.display_information();
+  BankAccount david("David J. Malan", 120);
+  david.display_information();
+
+  david.deposit(10);
+  david.display_information();
+  david.withdraw(120);
+  david.display_information();
+  david.withdraw(20);
+  david.display_information();
+  david.deposit(-12);
+  david.display_information();
 
   return 0;
 }
