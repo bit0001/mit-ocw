e12d8ab30947f479ed54a748a29c63d119ee5186
M <lmiguelvargasf@gmail.com>
2016-09-15 21:52:05 -0500

    Test getters

--- 6.088/playground/animal/test.cpp
+++ 6.088/playground/animal/test.cpp
@@ -5,6 +5,8 @@ int main() {
   Animal cat;
   cat.eat();
   cat.sleep();
+  std::cout << "This is" << cat.get_name() << "empty" << std::endl;
+  std::cout << "This is" << cat.get_age() << "empty" << std::endl;
 
   return 0;
 }
