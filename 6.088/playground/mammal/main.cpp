223e7dcef19cd576f864403f1c580a29f118a710
M <lmiguelvargasf@gmail.com>
2016-09-16 20:36:03 -0500

    Test classes

--- 6.088/playground/mammal/main.cpp
+++ 6.088/playground/mammal/main.cpp
@@ -1,6 +1,24 @@
 #include <iostream>
+#include "mammal.h"
+#include "dog.h"
+#include "cat.h"
+#include "human.h"
 
 int main() {
-  std::cout << "Hello, World!" << std::endl;
+  Human human;
+  human.breathe();
+  human.talk();
+  human.walk();
+
+  Cat cat;
+  cat.breathe();
+  cat.talk();
+  cat.walk();
+
+  Dog dog;
+  dog.breathe();
+  dog.talk();
+  dog.walk();
+
   return 0;
 }
