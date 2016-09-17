9e0ac3b84dce8a4396cc7801ec594f7f6c4bc2d8
M <lmiguelvargasf@gmail.com>
2016-09-16 21:17:07 -0500

    Define and implement class Player

---
+++ 6.088/playground/memory-allocation/player.cpp
@@ -0,0 +1,9 @@
+#include "player.h"
+
+Player::Player() {
+  std::cout << "Player born!" << std::endl;
+}
+
+Player::~Player() {
+  std::cout << "Player died!" << std::endl;
+}
---
+++ 6.088/playground/memory-allocation/player.h
@@ -0,0 +1,14 @@
+#ifndef PLAYER_H_
+#define PLAYER_H_
+
+#include <iostream>
+
+class Player {
+  std::string name;
+  int hp;
+public:
+  Player();
+  ~Player();
+};
+
+#endif//PLAYER_H_
